25a9adfd15729db592310f17ae3828a1692c3e3f
Agustin Pina <aguspina87@gmail.com>
2015-03-10 23:19:23 -0300

    Fix test

--- 011_existsAny/test.hs
+++ 011_existsAny/test.hs
@@ -1,6 +1,6 @@
 describe "existsAny" $ do
   it "devuelve False dados even y (1,3,5)" $ do
-    existsAny even (1,3,5) `shouldBe` True
+    existsAny even (1,3,5) `shouldBe` False
 
   it "devuelve True dados (0>) y (1,-3,7)" $ do
     existsAny (0>) (1,-3,7) `shouldBe` True
