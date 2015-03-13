f0ce9538fca507de3901ff720e559d2c4d5089fc
Agustin Pina <aguspina87@gmail.com>
2015-03-13 19:28:10 -0300

    Refactor tests

--- 022_frecuenciaCardiaca/test.hs
+++ 022_frecuenciaCardiaca/test.hs
@@ -1,14 +1,12 @@
 frecuenciaCardiaca = [80, 100, 120, 128, 130, 123, 125]
 
 
-describe "promedioFrecuenciaCardiaca," $ do
+describe "frecuencias" $ do
   it "devuelve 115.285714285714" $ do
     promedioFrecuenciaCardiaca `shouldBe` 115.285714285714
 
-describe "frecuenciaCardiacaMinuto," $ do
   it "devuelve 128 dado 30" $ do
     frecuenciaCardiacaMinuto 30 `shouldBe` 80
 
-describe "frecuenciasHastaMomento 30" $ do
   it "devuelve [80, 100, 120, 128] dado" $ do
     frecuenciasHastaMomento `shouldBe` [80, 100, 120, 128]
