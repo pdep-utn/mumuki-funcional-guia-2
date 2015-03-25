87296757faa9b1060e97af1429b38e76cbed8b78
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-03-24 23:25:13 -0300

    Update test.hs

--- 022_frecuenciaCardiaca/test.hs
+++ 022_frecuenciaCardiaca/test.hs
@@ -1,22 +1,22 @@
 describe "frecuencias" $ do
-  it "devuelve 115.285714285714" $ do
-    promedioFrecuenciaCardiaca `shouldBe` 115.285714285714
+  it "promedioFrecuenciaCardiaca == 115.285714285714" $ do
+    promedioFrecuenciaCardiaca `shouldBe` 115.14285714285714
 
-  it "devuelve 128 dado 30" $ do
+  it "frecuenciaCardiacaMomento 30 == 128" $ do
     frecuenciaCardiacaMomento 30 `shouldBe` 128
 
-  it "devuelve 120 dado 20" $ do
+  it "frecuenciaCardiacaMomento 20 == 120" $ do
     frecuenciaCardiacaMomento 20 `shouldBe` 120
 
-  it "devuelve 80 dado 0" $ do
+  it "frecuenciaCardiacaMomento 0 == 80" $ do
     frecuenciaCardiacaMomento 0 `shouldBe` 80
 
-  it "devuelve [80, 100, 120, 128] dado 30" $ do
+  it "frecuenciasHastaMomento 30 == [80, 100, 120, 128]" $ do
     frecuenciasHastaMomento 30 `shouldBe` [80, 100, 120, 128]
 
-  it "devuelve [80, 100, 120] dado 20" $ do
+  it "frecuenciasHastaMomento 20 == [80, 100, 120]" $ do
     frecuenciasHastaMomento 20 `shouldBe` [80, 100, 120]
 
-  it "devuelve [80] dado 0" $ do
+  it "frecuenciasHastaMomento 0 == [80]" $ do
     frecuenciasHastaMomento 0 `shouldBe` [80]
 
