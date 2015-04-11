d9a1a730554f1712800e59ba311143a89e3b8458
iluetich <ivan.luetich93@gmail.com>
2015-04-11 14:15:48 -0300

    Modified test to match exercise description

--- 011_existsAny/test.hs
+++ 011_existsAny/test.hs
@@ -1,9 +1,5 @@
 describe "existsAny" $ do
-  it "hay al menos un número par en [1,3,5,7,11,20]" $ do
-    existsAny even [1,3,5,7,11,20] `shouldBe` True
-  it "hay al menos un número impar en [1,2,10,40,30,100]" $ do
-  	existsAny odd [1,2,10,40,30,100] `shouldBe` True
-  it "hay al menos una palabra de más de 5 letras en ["hola", "murcielago", "cinco"]" $ do
-  	existsAny (\x -> length x > 5) ["hola", "murcielago", "cinco"] `shouldBe` True
-  it "no existen números pares en []" $ do
-  	existsAny even [] `shouldBe` False
+  it "hay al menos un número par en (1,3,4)" $ do
+  	existsAny even (1,3,4) `shouldbe` True
+  it "no hay números pares en (1,5,27)" $ do
+  	existsAny even (1,5,27) `shouldbe` False
