f483e36fc74635482e635f47247b55b353f6063f
iluetich <ivan.luetich93@gmail.com>
2015-04-11 14:17:10 -0300

    Fix typo

--- 011_existsAny/test.hs
+++ 011_existsAny/test.hs
@@ -1,5 +1,5 @@
 describe "existsAny" $ do
   it "hay al menos un número par en (1,3,4)" $ do
-  	existsAny even (1,3,4) `shouldbe` True
+  	existsAny even (1,3,4) `shouldBe` True
   it "no hay números pares en (1,5,27)" $ do
-  	existsAny even (1,5,27) `shouldbe` False
+  	existsAny even (1,5,27) `shouldBe` False
