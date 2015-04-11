f96340f67f8c60c05a1591cda7938facb4433192
iluetich <ivan.luetich93@gmail.com>
2015-04-11 14:11:53 -0300

    Fix test exercise 011_existsAny

--- 011_existsAny/test.hs
+++ 011_existsAny/test.hs
@@ -1,3 +1,9 @@
-describe "promediosSinAplazos" $ do
-  it "devuelve [7,5] dados y [[5,9],[6,2,4]]" $ do
-    promediosSinAplazos [[5,9],[6,2,4]] `shouldBe` [7,5]
+describe "existsAny" $ do
+  it "hay al menos un número par en [1,3,5,7,11,20]" $ do
+    existsAny even [1,3,5,7,11,20] `shouldBe` True
+  it "hay al menos un número impar en [1,2,10,40,30,100]" $ do
+  	existsAny odd [1,2,10,40,30,100] `shouldBe` True
+  it "hay al menos una palabra de más de 5 letras en ["hola", "murcielago", "cinco"]" $ do
+  	existsAny (\x -> length x > 5) ["hola", "murcielago", "cinco"] `shouldBe` True
+  it "no existen números pares en []" $ do
+  	existsAny even [] `shouldBe` False
