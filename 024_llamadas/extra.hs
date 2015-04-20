844e1cd162f4c09e622e7078bbdbc66a0ed9be25
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-19 22:33:11 -0300

    Corrigiendo commillas

--- 024_llamadas/extra.hs
+++ 024_llamadas/extra.hs
@@ -1,1 +1,1 @@
-duracionLlamadas = (("horarioReducido", [20,10,25,15]), (“horarioNormal”, [10,5,8,2,9,10]))
+duracionLlamadas = (("horarioReducido", [20,10,25,15]), ("horarioNormal", [10,5,8,2,9,10]))
