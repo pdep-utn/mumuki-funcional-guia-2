56d10dc8d243a7f889adbcdb889e5852d70bdbeb
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-25 15:03:00 -0300

    Tipando explicitamente una lista vacía

--- 310_aplicarFunciones/test.hs
+++ 310_aplicarFunciones/test.hs
@@ -3,7 +3,7 @@ describe "aplicarFunciones" $ do
     aplicarFunciones [succ,negate,abs] 10 `shouldBe` [11,-10,10]
 
   it "aplicarFunciones [] \"hola\" == []" $ do
-    aplicarFunciones [] "hola" `shouldBe` []
+    aplicarFunciones [] "hola" `shouldBe` ([]::[String])
 
   it "aplicarFunciones [length] \"hola\" == [4]" $ do
     aplicarFunciones [length] "hola" `shouldBe` [4]
