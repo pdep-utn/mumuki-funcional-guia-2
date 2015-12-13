cbe79c5d4d196dc43cecd13f193aa2f0d5b65849
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-13 13:07:18 -0300

    Removiendo ejercicios sin listas

--- 013_aplicarPar/test.hs
+++
@@ -1,6 +0,0 @@
-describe "aplicarPar" $ do
-  it "devuelve False dados even y (1,3,5)" $ do
-    aplicarPar (*2) (3,12) `shouldBe` (6,24)
-
-  it "devuelve True dados (0>) y (1,-3,7)" $ do
-    aplicarPar even (3,12) `shouldBe` (False,True)
